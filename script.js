18e2eb883ce8a189e95d9e92e330f46d615cfb5c
haileyrs <haileyscheevel@gmail.com>
2023-04-11 16:02:38 -0500

    convert flex box to grid, add button functionality

--- script.js
+++ script.js
@@ -1,36 +1,84 @@
 // create grid
 // adjust box sizing accordingly
 
+const gridSpace = 600
+let currentMode = 'singleBtn'
+let currentSize = 16
 
-let displayBox = document.querySelector('.display-box');
-let grid = document.getElementById('grid');
+// grid
+const displayBox = document.querySelector('.display-box');
+const grid = document.getElementById('grid');
 
-function windowSizing() {
-  let width = window.innerWidth;
-  let height = window.innerHeight;
+// buttons
+const singleBtn = document.querySelector('.single-mode');
+const rainbowBtn = document.querySelector('.rainbow-mode');
+const eraserBtn = document.querySelector('.eraser-mode');
+const clearBtn = document.querySelector('.clear-mode');
+const slider = document.querySelector('#sizer');
+const sizeVal = document.querySelector('#sizeVal');
 
-  if (width < 1028) {
-    let displayBoxDim = 'height: ' + (height-128) + 'px; width: ' + (width-128) + 'px;'  
-    let gridDim = 'height: ' + (height-196) + 'px; width: ' + (width-196) + 'px;'
-    displayBox.setAttribute('style', displayBoxDim)
-    grid.setAttribute('style', gridDim)
-  }
+singleBtn.addEventListener('click', () => changeMode('singleBtn'));
+rainbowBtn.addEventListener('click', () => changeMode('rainbowBtn'));
+eraserBtn.addEventListener('click', () => changeMode('eraserBtn'));
+clearBtn.addEventListener('click', () => clearAll());
+slider.addEventListener('input', () => changeSize());
+
+function changeMode(newMode) {
+  switch (currentMode) {
+    case 'singleBtn':
+      singleBtn.classList.remove('active')
+      break
+    case 'rainbowBtn':
+      rainbowBtn.classList.remove('active')
+      break
+    case 'eraserBtn':
+      eraserBtn.classList.remove('active')
+      break
+  };
+
+  switch (newMode) {
+    case 'singleBtn':
+      singleBtn.classList.add('active')
+      break
+    case 'rainbowBtn':
+      rainbowBtn.classList.add('active')
+      break
+    case 'eraserBtn':
+      eraserBtn.classList.add('active')
+      break
+  };
+
+  currentMode = newMode;
 }
 
+function clearAll() {
+  clearBtn.classList.add('active');
+  changeMode('singleBtn');
+  setTimeout(() => {
+    clearBtn.classList.remove('active');
+  }, 500); 
+}
+
+function changeSize() {
+  currentSize = sizer.value;
+  sizeVal.innerHTML = `${sizer.value}x${sizer.value}`
+  createGrid(sizer.value);
+}
 
 function createGrid(dimensions = 16) {
+  clearAll();
+  grid.innerHTML = '';
   if (dimensions < 100) {
-    // update grid size and grid square size based on dimesions and account for border width 1
-    
-    
+    // update grid size and grid square size based on dimesions 
+    let gridDim = `grid-template-columns: repeat(${dimensions}, 1fr); grid-template-rows: repeat(${dimensions}, 1fr);`
+    grid.setAttribute('style', gridDim)
+
     for (let i = 0; i < (dimensions*dimensions); i++) {
       const gridSquare = document.createElement('div');
       gridSquare.classList.add('grid-square');
-      gridSquare.setAttribute('style', '')
       grid.appendChild(gridSquare);
     }
   }
 }
-window.onresize = windowSizing()
 
-
+createGrid()
